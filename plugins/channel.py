d176fc74b828ef4642fc177d2b399a504ff33e6e
sourav8476 <souravbosu2178@gmail.com>
2024-10-07 01:15:58 +0600

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -76,7 +76,7 @@ async def send_movie_updates(bot, file_name, caption, file_id):
             return 
         processed_movies.add(movie_name)    
         poster_url = await get_imdb(movie_name)
-        caption_message = f"#New_File_Added ✅\n\nFile_Name:- <code>{movie_name}</code>\n\nLanguage:- {language}\n\nQuality:- {quality}"    
+        caption_message = f"#New_File_Added ✅\n\n🍿 File_Name:- <code>{movie_name}</code>\n\n🎙 Language:- {language}\n\n‼️ Quality:- {quality}\n🔎 𝐒𝐞𝐚𝐫𝐜𝐡 𝐨𝐧 :- @movierequestgroupHQ"    
         movie_update_channel = await db.movies_update_channel_id()    
         btn = [
             [InlineKeyboardButton('Get File', url=f'https://t.me/{temp.U_NAME}?start=pm_mode_file_{ADMINS[0]}_{file_id}')]
