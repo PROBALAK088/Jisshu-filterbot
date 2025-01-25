e126fb0883862cb742e014080d3dfc0502a9bfe9
sourav8476 <souravbosu2178@gmail.com>
2025-01-25 12:17:30 +0600

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -89,7 +89,7 @@ async def send_movie_updates(bot, file_name, caption, file_id):
         movie_update_channel = await db.movies_update_channel_id()    
         
         btn = [
-            [InlineKeyboardButton('🎥 𝗚𝗲𝘁 𝗙𝗶𝗹𝗲 🎥', url=f'https://telegram.me/LuxeFlixbot?start=getfile-{file_name}')],
+            [InlineKeyboardButton('🎥 𝗚𝗲𝘁 𝗙𝗶𝗹𝗲 🎥', url=f'https://t.me/{temp.U_NAME}?start=pm_mode_file_{ADMINS[0]}_{file_id}')],
             [InlineKeyboardButton('💫Mᴏᴠɪᴇ Sᴇᴀʀᴄʜ Gʀᴏᴜᴘ💝', url='https://t.me/movierequestgroupHQ')]
         ]
         reply_markup = InlineKeyboardMarkup(btn)
